745b8cc99f674c5849a5d78e9a3359a0e7b9bb56
aditiu <aditi.18.coool@gmail.com>
2018-04-20 15:40:42 +0530

    changinf file to verify poll

--- JenkinEclipseGit1/src/com/aditi/TestGit.java
+++ JenkinEclipseGit1/src/com/aditi/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
      
 		System.out.println("This code is coming from eclipse git");
+		System.out.println("This changed has been encountered from git poll");
 
 	}
 
